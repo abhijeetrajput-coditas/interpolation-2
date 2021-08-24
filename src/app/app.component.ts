23b2c2c2c6fa7a83b0e897cb0a69c51bff3fad02
Abhijeet Rajput <abhijeet.rajput@coditas.com>
2021-08-24 11:38:59 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +6,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'interpolation';
-}
+  serverId: number = 10;
+  serverName: string = "Cod;tas";
+  serverStatus: string = "offline";
+  designation = [{name: "Sr.1", designation:" Jr Software Associate"},
+                 {name: "Sr.2", designation:"Senior Software Associate"},
+                 {name: "Sr.3", designation:"Senior Software Associate ( Tech Lead )"}
+]}
